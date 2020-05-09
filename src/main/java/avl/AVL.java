e73bb19a4e1ef11cba8df8e0616c1648595880f1
Huy Nhat Ngo <ngoh@wwu.edu>
2020-05-09 00:47:49 -0700

    test 00 test

--- src/main/java/avl/AVL.java
+++ src/main/java/avl/AVL.java
@@ -1,138 +1,204 @@
 package avl;
 
+import org.w3c.dom.Node;
+
 public class AVL {
 
-  public Node root;
+    public Node root;
 
-  private int size;
+    private int size;
 
-  public int getSize() {
-    return size;
-  }
-
-  /** find w in the tree. return the node containing w or
-  * null if not found */
-  public Node search(String w) {
-    return search(root, w);
-  }
-  private Node search(Node n, String w) {
-    if (n == null) {
-      return null;
-    }
-    if (w.equals(n.word)) {
-      return n;
-    } else if (w.compareTo(n.word) < 0) {
-      return search(n.left, w);
-    } else {
-      return search(n.right, w);
-    }
-  }
-
-  /** insert w into the tree as a standard BST, ignoring balance */
-  public void bstInsert(String w) {
-    if (root == null) {
-      root = new Node(w);
-      size = 1;
-      return;
-    }
-    bstInsert(root, w);
-  }
-
-  /* insert w into the tree rooted at n, ignoring balance
-   * pre: n is not null */
-  private void bstInsert(Node n, String w) {
-    // TODO
-  }
-
-  /** insert w into the tree, maintaining AVL balance
-  *  precondition: the tree is AVL balanced */
-  public void avlInsert(String w) {
-    // TODO
-  }
-
-  /* insert w into the tree, maintaining AVL balance
-   *  precondition: the tree is AVL balanced and n is not null */
-  private void avlInsert(Node n, String w) {
-    // TODO
-  }
-
-  /** do a left rotation: rotate on the edge from x to its right child.
-  *  precondition: x has a non-null right child */
-  public void leftRotate(Node x) {
-    // TODO
-  }
-
-  /** do a right rotation: rotate on the edge from x to its left child.
-  *  precondition: y has a non-null left child */
-  public void rightRotate(Node y) {
-    // TODO
-  }
-
-  /** rebalance a node N after a potentially AVL-violoting insertion.
-  *  precondition: none of n's descendants violates the AVL property */
-  public void rebalance(Node n) {
-    // TODO
-  }
-
-  /** remove the word w from the tree */
-  public void remove(String w) {
-    remove(root, w);
-  }
-
-  /* remove v from the tree rooted at n */
-  private void remove(Node n, String w) {
-    return; // (enhancement TODO - do the base assignment first)
-  }
-
-  /** print a sideways representation of the tree - root at left,
-  * right is up, left is down. */
-  public void printTree() {
-    printSubtree(root, 0);
-  }
-  private void printSubtree(Node n, int level) {
-    if (n == null) {
-      return;
-    }
-    printSubtree(n.right, level + 1);
-    for (int i = 0; i < level; i++) {
-      System.out.print("        ");
-    }
-    System.out.println(n);
-    printSubtree(n.left, level + 1);
-  }
-
-  /** inner class representing a node in the tree. */
-  public class Node {
-    public String word;
-    public Node parent;
-    public Node left;
-    public Node right;
-    public int height;
-
-    public String toString() {
-      return word + "(" + height + ")";
+    public int getSize() {
+        return size;
     }
 
-    /** constructor: gives default values to all fields */
-    public Node() { }
-
-    /** constructor: sets only word */
-    public Node(String w) {
-      word = w;
+    /**
+     * find w in the tree. return the node containing w or
+     * null if not found
+     */
+    public org.w3c.dom.Node search(String w) {
+        return search(root, w);
     }
 
-    /** constructor: sets word and parent fields */
-    public Node(String w, Node p) {
-      word = w;
-      parent = p;
+    private org.w3c.dom.Node search(org.w3c.dom.Node n, String w) {
+        if (n == null) {
+            return null;
+        }
+        if (w.equals(n.word)) {
+            return n;
+        } else if (w.compareTo(n.word) < 0) {
+            return search(n.left, w);
+        } else {
+            return search(n.right, w);
+        }
     }
 
-    /** constructor: sets all fields */
-    public Node(String w, Node p, Node l, Node r) {
-      word = w;
-      parent = p;
-      left = l;
-      right = r;
+    /**
+     * insert w into the tree as a standard BST, ignoring balance
+     */
+    public void bstInsert(String w) {
+        if (root == null) {
+            root = new org.w3c.dom.Node(w);
+            size = 1;
+            return;
+        }
+        bstInsert(root, w);
     }
-  }
+
+    /* insert w into the tree rooted at n, ignoring balance
+     * pre: n is not null */
+    private void bstInsert(Node n, String w) {
+        // TODO
+        Node current = n;
+        Node parent = null;
+        //null root
+        if (n == null)
+            n = new Node(w);
+
+        while (current != null) {
+            if (w.compareTo(current.word) < 0) {
+                parent = current;
+                current = current.left;
+            } else if (w.compareTo(current.word) > 0) {
+                parent = current;
+                current = current.right;
+            } else
+                break;
+        }
+
+        if (current != null)
+            return;
+        else {
+            if (w.compareTo(parent.word) > 0) {
+                parent.right.word = w;
+                size++;
+            } else {
+                parent.left.word = w;
+                size++;
+            }
+        }
+    }
+
+    /**
+     * insert w into the tree, maintaining AVL balance
+     * precondition: the tree is AVL balanced
+     */
+    public void avlInsert(String w) {
+        // TODO
+    }
+
+    /* insert w into the tree, maintaining AVL balance
+     *  precondition: the tree is AVL balanced and n is not null */
+    private void avlInsert(Node n, String w) {
+        // TODO
+    }
+
+    /**
+     * do a left rotation: rotate on the edge from x to its right child.
+     * precondition: x has a non-null right child
+     */
+    public void leftRotate(Node x) {
+        // TODO
+    }
+
+    /**
+     * do a right rotation: rotate on the edge from x to its left child.
+     * precondition: y has a non-null left child
+     */
+    public void rightRotate(Node y) {
+        // TODO
+    }
+
+    /**
+     * rebalance a node N after a potentially AVL-violoting insertion.
+     * precondition: none of n's descendants violates the AVL property
+     */
+    public void rebalance(Node n) {
+        // TODO
+    }
+
+    /**
+     * remove the word w from the tree
+     */
+    public void remove(String w) {
+        remove(root, w);
+    }
+
+    /* remove v from the tree rooted at n */
+    private void remove(Node n, String w) {
+        return; // (enhancement TODO - do the base assignment first)
+    }
+
+    /**
+     * print a sideways representation of the tree - root at left,
+     * right is up, left is down.
+     */
+    public void printTree() {
+        printSubtree(root, 0);
+    }
+
+    private void printSubtree(Node n, int level) {
+        if (n == null) {
+            return;
+        }
+        printSubtree(n.right, level + 1);
+        for (int i = 0; i < level; i++) {
+            System.out.print("        ");
+        }
+        System.out.println(n);
+        printSubtree(n.left, level + 1);
+    }
+
+    /**
+     * inner class representing a node in the tree.
+     */
+    public class Node {
+        public String word;
+        public Node parent;
+        public Node left;
+        public Node right;
+        public int height;
+
+        public String toString() {
+            return word + "(" + height + ")";
+        }
+
+        /**
+         * constructor: gives default values to all fields
+         */
+        public Node() {
+        }
+
+        /**
+         * constructor: sets only word
+         */
+        public Node(String w) {
+            word = w;
+        }
+
+        /**
+         * constructor: sets word and parent fields
+         */
+        public Node(String w, Node p) {
+            word = w;
+            parent = p;
+        }
+
+        /**
+         * constructor: sets all fields
+         */
+        public Node(String w, Node p, Node l, Node r) {
+            word = w;
+            parent = p;
+            left = l;
+            right = r;
+        }
+    }
+}
+
+class testAVL {
+    public static void main(String[] args) {
+        String s = "abc";
+        System.out.println("checking: " + s.compareTo("abcd"));
+    }
 }
